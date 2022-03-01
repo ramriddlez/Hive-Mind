a095f693fefe83d370edef394cb99822776b5694
Bemnet Dejene <bdejene19@gmail.com>
2022-03-01 14:00:07 -0500

    controllers

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,6 +1,9 @@
 const app = require("express").Router();
+
 const homeRoutes = require("./homeRoute");
 const apiRoutes = require("./api/index");
+
 app.use("/", homeRoutes);
 app.use("/api", apiRoutes);
+
 module.exports = app;
