bfa6f5fce937a6d094f7d7dadb02238ed8130fcb
ramriddlez <ramankangz@outlook.com>
2022-03-02 11:42:01 -0500

    typo fix

--- Develop/controllers/routes/index.js
+++ Develop/controllers/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const homeRoute = require('./homeRoute');
 
-router.use('/readers', readerRoutes);
-router.use('/cards', libraryCardRoutes);
+router.use('/users', userRoutes);
+router.use('/??', homeRoute);
 
 module.exports = router;
