af00e2f367d67463a60f0f8aa5d1b4cce9df8283
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-03-03 18:33:13 -0500

    made tipsRoutes funcitonal, added nodemon, started profileRoutes

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
-const userRoutes = require('./userRoutes')
+const userRoutes = require('./userRoutes');
+const tipRoutes = require('./tipRoutes');
 
 router.use('/users', userRoutes);
+router.use('/tips', tipRoutes);
 
 module.exports = router;
