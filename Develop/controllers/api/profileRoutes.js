1eb96b1bcca64d3838e90cbe3a273a2175eeb209
Kenneth MacLean <kenneth.maclean84@gmail.com>
2022-03-03 21:29:47 -0500

    added profileRoutes

---
+++ Develop/controllers/api/profileRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id);
+      if (!userData) {
+        res.status(404).json({ message: 'No user with this id!' });
+        return;
+      }
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+module.exports = router;
