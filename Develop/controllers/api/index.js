ac7cdf976cbaddc3e7d7191a888c0c8d04dfcaba
Bemnet Dejene <bdejene19@gmail.com>
2022-03-03 20:50:42 -0500

    added tipRoutes to main index js file. Created (but not finished) basic tip route for frontend random quote generator

--- Develop/controllers/api/index.js
+++ Develop/controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const userRoutes = require('./userRoutes')
-
-router.use('/users', userRoutes);
-
+const userRoutes = require("./userRoutes");
+const tipRoutes = require("./tipRoutes");
+router.use("/users", userRoutes);
+router.use("/tips", tipRoutes);
 module.exports = router;
