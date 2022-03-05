df2833ec687d78f602407eee7d4f5647a39ce11b
Bemnet Dejene <bdejene19@gmail.com>
2022-03-05 12:16:53 -0500

    created helper functions to keep code DRY. Helpers contain model requests to get all tips, users, user by email, and can add as we go

--- Develop/utils/helpers.js
+++ Develop/utils/helpers.js
@@ -1,6 +1,35 @@
 //uuid random generator code for daily TIP insertions..
+const { User, Tip } = require("../models/index");
+const getAllTips = async () => {
+  let allPosts = await Tip.findAll({});
+  return allPosts;
+};
 
-module.exports = () =>
-  Math.floor((1 + Math.random()) * 0x10000)
-    .toString(16)
-    .substring(1);
+const getAllUsers = async () => {
+  let allUsers = await User.findAll({
+    attributes: ["name", "email"],
+    include: [{ model: Tip }],
+  });
+  return allUsers;
+};
+const findUserByEmail = async (email) => {
+  let specificUser = await User.findOne({
+    attributes: ["name", "email"],
+    include: [{ model: Tip }],
+    where: {
+      email: email,
+    },
+  });
+  return specificUser;
+};
+
+module.exports = {
+  getAllTips,
+  getAllUsers,
+  findUserByEmail,
+};
+
+// module.exports = () =>
+//   Math.floor((1 + Math.random()) * 0x10000)
+//     .toString(16)
+//     .substring(1);
