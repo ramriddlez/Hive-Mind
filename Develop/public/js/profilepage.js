beb8ba7cdd63ba7704cc68e36d3a7d82104f7675
gammahazard <92896466+gammahazard@users.noreply.github.com>
2022-03-05 19:28:47 -0800

    fix grammar

--- Develop/public/js/profilepage.js
+++ Develop/public/js/profilepage.js
@@ -84,7 +84,3 @@ $(document).ready(function(){
       $('#dogImage').attr('src', data.message)
     });
   }
-  $(".btn").on("click",function(){
-    $(".input").toggleClass("inclicked");
-    $(".btn").toggleClass("close");
-  })
