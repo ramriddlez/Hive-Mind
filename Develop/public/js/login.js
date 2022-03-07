005f5801667fc34872b7c4e1f29607de1c8517c8
ramriddlez <ramankangz@outlook.com>
2022-03-07 10:35:51 -0500

    added captcha again with fix

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -37,11 +37,8 @@ const signupFormHandler = async (event) => {
 
     if (response.ok) {
       // document.location.replace('/');
-      alert("ACCOUNT CREATED");
-    } else {
-      alert("Failed to sign up.");
-    }
-  }
+      cap();
+    }} 
 };
 
 document
@@ -51,3 +48,29 @@ document
 document
   .querySelector("#signup-button")
   .addEventListener("click", signupFormHandler);
+
+  function cap(){
+    var alpha = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V'
+                 ,'W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i',
+                 'j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', '!','@','#','$','%','^','&','*','+'];
+                 var a = alpha[Math.floor(Math.random()*71)];
+                 var b = alpha[Math.floor(Math.random()*71)];
+                 var c = alpha[Math.floor(Math.random()*71)];
+                 var d = alpha[Math.floor(Math.random()*71)];
+                 var e = alpha[Math.floor(Math.random()*71)];
+                 var f = alpha[Math.floor(Math.random()*71)];
+
+                 var final = a+b+c+d+e+f;
+                 document.getElementById("capt").value=final;
+               }
+               function validcap(){
+                var stg1 = document.getElementById('capt').value;
+                var stg2 = document.getElementById('textinput').value;
+                if(stg1==stg2){
+                  alert("Form is validated Succesfully");
+                  return true;
+                }else{
+                  alert("Please enter a valid captcha");
+                  return false;
+                }
+               }
